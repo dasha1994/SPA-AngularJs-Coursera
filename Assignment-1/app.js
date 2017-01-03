a3525d134f2bd103841691e31b3ec8f497d0dfb2
dasha1994 <kozy.dasha@gmail.com>
2017-01-03 17:13:53 +0200

    commit assignment

---
+++ Assignment-1/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+	$scope.items = null;
+	$scope.message = "Enjoy!";
+	$scope.CheckIfTooMuch = function(){
+		console.log("hop hey");
+		console.log($scope.items.split(',').length);
+       if($scope.items.split(',').length>3){
+       	$scope.message = "Too much";
+       }else{
+       		$scope.message = "Enjoy!";
+       }
+	};
+};
+
+
+
+
+
+})();
